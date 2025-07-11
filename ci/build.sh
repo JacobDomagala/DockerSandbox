137f757c950910dccf5b4e6942e86c0319d638e2
Jacob Domagala <domagala.jacob@gmail.com>
2025-07-12 00:05:04 +0200

    Add inputs to build script

--- ci/build.sh
+++ ci/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
+source_dir=$1
+build_dir=$2
 
-mkdir -p build
-cd build || exit 1
+mkdir -p "${build_dir}"
+cd "${build_dir}" || exit 1
 rm -rf ./*
 
-cmake -DCMAKE_CXX_COMPILER="ccache g++" \
-      ..
+cmake -DCMAKE_CXX_COMPILER_LAUNCHER=ccache "${source_dir}"
 make
-
 
 ccache -s
