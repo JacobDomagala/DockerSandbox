4a6f9dd665f0799025e7fae79c37d00e18703633
Jacob Domagala <domagala.jacob@gmail.com>
2025-07-13 16:56:12 +0200

    Another one

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,7 @@ int main(int argc, char* argv[]) {
     // Perform some sample calculations
     std::vector<std::pair<double, double>> testCases = {
         {10.0, 5.0},
-        {25.0, 4.0},
+        {35.0, 4.0},
         {120.0, 11.0},
         {3.14159,32.0}
     };
